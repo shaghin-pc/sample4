4aaaaaddf23f96f722a7a8d1a773a94342c5db98
shaghin-pc <shaginpc143@gmail.com>
2019-12-06 11:16:54 +0530

    fenish

--- app/src/main/java/com/shagi/sample4/MainActivity.java
+++ app/src/main/java/com/shagi/sample4/MainActivity.java
@@ -3,12 +3,32 @@ package com.shagi.sample4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText  ed1,ed2;
+    Button bt1;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.e1);
+        ed2=(EditText)findViewById(R.id.e2);
+        bt1=(Button)findViewById(R.id.b1);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"ada fenesh aayi",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
